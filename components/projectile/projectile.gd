d649ab42b35895d83ad3bc2eaa88cfe1f8b43f06
rgdotta <rafaelgdotta@gmail.com>
2025-11-14 17:33:54 -0300

    Fix projectile collision

--- components/projectile/projectile.gd
+++ components/projectile/projectile.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 class_name Projectile
 
 @export var speed := 400.0
@@ -6,20 +6,19 @@ class_name Projectile
 @onready var duration_timer: Timer = $DurationTimer
 @onready var hitbox: Hitbox = $Hitbox
 
-var direction: Vector2 = Vector2.ZERO
+var direction := Vector2.ZERO
 
 func _ready() -> void:
 	duration_timer.start(0.5)
 
-func _process(delta: float) -> void:
-	global_position += direction * speed * delta
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(direction * speed  * delta)
+	if collision:
+		queue_free()
 
 func launch(dir: Vector2) -> void:
 	direction = dir.normalized()
 
-func _on_duration_timer_timeout() -> void:
-	queue_free()
-	
 func set_enemy_projectile():
 	hitbox.set_collision_layer_value(3, false)
 	
@@ -28,3 +27,4 @@ func set_player_projectile():
 
 func _on_hitbox_hit(hurtbox: Variant) -> void:
 	queue_free()
+	
